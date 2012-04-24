3e732493d8bb8f652b545de33331c26c187c9ab1
Caroline Pantofaru <devnull@localhost>
2012-04-24 12:45:37 -0700

    adding faces to robot launch and finding the closest face

--- slider_gui/src/actions/Pr2LookAtFace.py
+++ slider_gui/src/actions/Pr2LookAtFace.py
@@ -21,12 +21,14 @@ class Pr2LookAtFace(Action):
         super(Pr2LookAtFace, self).__init__()
         self._client = SimpleActionClient('face_detector_action',FaceDetectorAction)
         self._client.wait_for_server()
+        self._head_client = SimpleActionClient('/head_traj_controller/point_head_action', PointHeadAction)
+        self._head_client.wait_for_server()
         self._timer = None
         #self.set_duration(duration)
 
     def to_string(self):
         #str = super(Pr2LookAtFace, self).to_string()
-        return 'look_for_face()'# % str
+        return 'look_at_face()'# % str
 
 #    def deepcopy(self):
 #        return Pr2LookAtFace(self.get_duration())
@@ -40,13 +42,21 @@ class Pr2LookAtFace(Action):
         self._client.wait_for_result()
         f = self._client.get_result()
         if len(f.face_positions) > 0:
-            g = PointHeadGoal()
-            g.target.header.frame_id = f.face_positions[0].header.frame_id
-            g.target.point.x = f.face_positions[0].pos.x
-            g.target.point.y = f.face_positions[0].pos.y
-            g.target.point.z = f.face_positions[0].pos.z
-            g.min_duration = rospy.Duration(1.0)
-            head_client.send_goal(g)
+            closest = -1
+            closest_dist = 1000
+            for i in range(len(f.face_positions)):
+                dist = f.face_positions[i].pos.x*f.face_positions[i].pos.x + f.face_positions[i].pos.y*f.face_positions[i].pos.y + f.face_positions[i].pos.z*f.face_positions[i].pos.z
+                if dist < closest_dist: 
+                    closest = i
+                    closest_dist = dist
+            if closest > -1:
+                g = PointHeadGoal()
+                g.target.header.frame_id = f.face_positions[closest].header.frame_id
+                g.target.point.x = f.face_positions[closest].pos.x
+                g.target.point.y = f.face_positions[closest].pos.y
+                g.target.point.z = f.face_positions[closest].pos.z
+                g.min_duration = rospy.Duration(1.0)
+                self._head_client.send_goal(g)
         self._finished_finding_face()
 
     def _preempt(self, event):
