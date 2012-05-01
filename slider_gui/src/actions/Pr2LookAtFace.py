6e59433930454d2507d48e2e062b70341b28e7ba
Dirk Thomas <dthomas@willowgarage.com>
2012-05-02 01:45:18 +0200

    removed face recognition stuff from simulator launch file and modified action to handle case when action server is not available

--- slider_gui/src/actions/Pr2LookAtFace.py
+++ slider_gui/src/actions/Pr2LookAtFace.py
@@ -17,26 +17,27 @@ import rospy
 
 class Pr2LookAtFace(Action):
 
-    def __init__(self):#, duration = 3.0):
+    def __init__(self):
         super(Pr2LookAtFace, self).__init__()
         self._client = SimpleActionClient('face_detector_action',FaceDetectorAction)
-        self._client.wait_for_server()
         self._head_client = SimpleActionClient('/head_traj_controller/point_head_action', PointHeadAction)
-        self._head_client.wait_for_server()
         self._timer = None
-        #self.set_duration(duration)
 
     def to_string(self):
-        #str = super(Pr2LookAtFace, self).to_string()
-        return 'look_at_face()'# % str
+        return 'look_at_face()'
 
     def execute(self):
         super(Pr2LookAtFace, self).execute()
         print('Pr2LookAtFace.execute() %d' % self.get_duration())
+        # delay execution to not run nested in the current stack context
         self._timer = rospy.Timer(rospy.Duration.from_sec(0.001), self._execute, oneshot=True)
 
     def _execute(self, event):
         self._timer = rospy.Timer(rospy.Duration.from_sec(self.get_duration()), self._preempt, oneshot=True)
+        connected = self._client.wait_for_server(rospy.Duration(1.0))
+        if not connected:
+            print('Pr2LookAtFace.execute() does nothing since face detector action was not found')
+            return
         fgoal = FaceDetectorGoal()
         self._client.send_goal(fgoal)
         self._client.wait_for_result()
@@ -56,7 +57,9 @@ class Pr2LookAtFace(Action):
                 g.target.point.y = f.face_positions[closest].pos.y
                 g.target.point.z = f.face_positions[closest].pos.z
                 g.min_duration = rospy.Duration(1.0)
-                self._head_client.send_goal(g)
+                connected = self._head_client.wait_for_server(rospy.Duration(1.0))
+                if connected:
+                    self._head_client.send_goal(g)
         self._finished_finding_face()
 
     def _preempt(self, event):
@@ -66,9 +69,3 @@ class Pr2LookAtFace(Action):
     def _finished_finding_face(self):
         self._timer.shutdown()
         self._execute_finished()
-
-    def _timer_finished(self, event):
-        self._timer = None
-        self._execute_finished()
-            
-    
