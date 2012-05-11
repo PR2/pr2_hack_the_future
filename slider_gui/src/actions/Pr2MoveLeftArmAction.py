8b168b85218b800184a120525d639fd309c85025
Dirk Thomas <dthomas@willowgarage.com>
2012-05-11 11:10:09 -0700

    undo reducing range of some joint by one degree

--- slider_gui/src/actions/Pr2MoveLeftArmAction.py
+++ slider_gui/src/actions/Pr2MoveLeftArmAction.py
@@ -4,10 +4,10 @@ class Pr2MoveLeftArmAction(Pr2JointTrajectoryAction):
 
     def __init__(self):
         super(Pr2MoveLeftArmAction, self).__init__('l_arm_controller/joint_trajectory_action')
-        self._add_joint('l_shoulder_pan_joint', 0, 129)
-        self._add_joint('l_shoulder_lift_joint', -29, 80)
+        self._add_joint('l_shoulder_pan_joint', 0, 130)
+        self._add_joint('l_shoulder_lift_joint', -30, 80)
         self._add_joint('l_upper_arm_roll_joint', -44, 224)
-        self._add_joint('l_elbow_flex_joint', -129, 0)
+        self._add_joint('l_elbow_flex_joint', -130, 0)
         self._add_joint('l_forearm_roll_joint', -180, 180)
         self._add_joint('l_wrist_flex_joint', -130, 0)
         self._add_joint('l_wrist_roll_joint', -180, 180)
--- slider_gui/src/actions/Pr2MoveRightArmAction.py
+++ slider_gui/src/actions/Pr2MoveRightArmAction.py
@@ -4,10 +4,10 @@ class Pr2MoveRightArmAction(Pr2JointTrajectoryAction):
 
     def __init__(self):
         super(Pr2MoveRightArmAction, self).__init__('r_arm_controller/joint_trajectory_action')
-        self._add_joint('r_shoulder_pan_joint', -129, 0)
-        self._add_joint('r_shoulder_lift_joint', -29, 80)
+        self._add_joint('r_shoulder_pan_joint', -130, 0)
+        self._add_joint('r_shoulder_lift_joint', -30, 80)
         self._add_joint('r_upper_arm_roll_joint', -224, 44)
-        self._add_joint('r_elbow_flex_joint', -129, 0)
+        self._add_joint('r_elbow_flex_joint', -130, 0)
         self._add_joint('r_forearm_roll_joint', -180, 180)
         self._add_joint('r_wrist_flex_joint', -130, 0)
         self._add_joint('r_wrist_roll_joint', -180, 180)
