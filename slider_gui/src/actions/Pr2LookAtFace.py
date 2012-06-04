8ace9d20ab2e1a49c863fd7389b1e96c73959b55
Caroline Pantofaru <devnull@localhost>
2012-06-04 16:56:45 -0700

    face detector fixes for continuous tracking. Also including it into the program_queue.launch file (again?)

--- slider_gui/src/actions/Pr2LookAtFace.py
+++ slider_gui/src/actions/Pr2LookAtFace.py
@@ -39,33 +39,44 @@ class Pr2LookAtFace(Action):
         # delay execution to not run nested in the current stack context
         self._timer = rospy.Timer(rospy.Duration.from_sec(0.001), self._execute, oneshot=True)
 
+
     def _execute(self, event):
         self._executing = True
         self._timer = rospy.Timer(rospy.Duration.from_sec(self.get_duration()), self._preempt, oneshot=True)
         hgoal = None
         connected = self._client.wait_for_server(rospy.Duration(1.0))
-        if connected and self._executing:
-            fgoal = FaceDetectorGoal()
-            self._pending_face_goal = True
-            self._client.send_goal(fgoal)
-            self._client.wait_for_result()
-            self._pending_face_goal = False
-            f = self._client.get_result()
-            if len(f.face_positions) > 0:
-                closest = -1
-                closest_dist = 1000
-                for i in range(len(f.face_positions)):
-                    dist = f.face_positions[i].pos.x*f.face_positions[i].pos.x + f.face_positions[i].pos.y*f.face_positions[i].pos.y + f.face_positions[i].pos.z*f.face_positions[i].pos.z
-                    if dist < closest_dist: 
-                        closest = i
-                        closest_dist = dist
-                if closest > -1:
-                    hgoal = PointHeadGoal()
-                    hgoal.target.header.frame_id = f.face_positions[closest].header.frame_id
-                    hgoal.target.point.x = f.face_positions[closest].pos.x
-                    hgoal.target.point.y = f.face_positions[closest].pos.y
-                    hgoal.target.point.z = f.face_positions[closest].pos.z
-                    hgoal.min_duration = rospy.Duration(1.0)
+        if connected:
+            while self._executing:
+                fgoal = FaceDetectorGoal()
+                self._pending_face_goal = True
+                self._client.send_goal(fgoal)
+                self._client.wait_for_result()
+                self._pending_face_goal = False
+                f = self._client.get_result()
+                if len(f.face_positions) > 0:
+                    closest = -1
+                    closest_dist = 1000
+                    for i in range(len(f.face_positions)):
+                        dist = f.face_positions[i].pos.x*f.face_positions[i].pos.x + f.face_positions[i].pos.y*f.face_positions[i].pos.y\
+ + f.face_positions[i].pos.z*f.face_positions[i].pos.z
+                        if dist < closest_dist:
+                            closest = i
+                            closest_dist = dist
+                    if closest > -1:
+                        hgoal = PointHeadGoal()
+                        hgoal.target.header.frame_id = f.face_positions[closest].header.frame_id
+                        hgoal.target.point.x = f.face_positions[closest].pos.x
+                        hgoal.target.point.y = f.face_positions[closest].pos.y
+                        hgoal.target.point.z = f.face_positions[closest].pos.z
+                        hgoal.min_duration = rospy.Duration(1.0)        
+                        if self._executing:
+                            hconnected = self._head_client.wait_for_server(rospy.Duration(1.0))
+                            if hconnected and self._executing:
+                                self._pending_head_goal = True
+                                self._head_client.send_goal(hgoal)
+                                #                            self._head_client.wait_for_result()
+                                #                            self._pending_head_goal = False
+
         else:
             hgoal = PointHeadGoal()
             hgoal.target.header.frame_id = "base_footprint";
@@ -73,17 +84,18 @@ class Pr2LookAtFace(Action):
             hgoal.target.point.y = -2.0
             hgoal.target.point.z = 1.2
             hgoal.min_duration = rospy.Duration(1.0)
+            if self._executing:
+                hconnected = self._head_client.wait_for_server(rospy.Duration(1.0))
+                if hconnected and self._executing:
+                    self._pending_head_goal = True
+                    self._head_client.send_goal(hgoal)
+                    self._head_client.wait_for_result()
+                    self._pending_head_goal = False
+                    if self._executing:
+                        time.sleep(1.0)
+        
+        self._finished_finding_face()
 
-        if hgoal is not None and self._executing:
-            connected = self._head_client.wait_for_server(rospy.Duration(1.0))
-            if connected and self._executing:
-                self._pending_head_goal = True
-                self._head_client.send_goal(hgoal)
-                self._head_client.wait_for_result()
-                self._pending_head_goal = False
-                if self._executing:
-                    time.sleep(1.0)
-            self._finished_finding_face()
 
     def _preempt(self, event):
         self._executing = False
