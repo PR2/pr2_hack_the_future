655a4b5715f34e3e0fedc201a576fc57999bb55f
Austin Hendrix <ahendrix@willowgarage.com>
2012-08-13 22:42:44 -0700

    New, Load and Save work.

--- queue_web/www/editor.js
+++ queue_web/www/editor.js
@@ -2,30 +2,49 @@ var editor;
 
 var programs;
 
-function load(f) {
-   var id = programs[f.program.selectedIndex].id;
-   connection.callService('/get_program', JSON.stringify([id]),
-         function(resp) {
-            editor.setValue(resp.program.code);
-            editor.clearSelection();
-            f.program_name.value = resp.program.info.name;
-         });
+// update the list of programs from the programs array
+function update_list(selection) {
+   var form = document.getElementById("program_info");
+   form.program.innerHTML = "";
+   for( var i = 0; i < programs.length; i++ ) {
+      var opt = document.createElement('option');
+      opt.text = "(" + programs[i].id + "): " + 
+         programs[i].name;
+      opt.value = opt.text;
+      form.program.add(opt, null);
+   }
+   form.program.selectedIndex = selection;
 }
 
+// load the program specified on the form
+function load(f) {
+   var sel = f.program.selectedIndex;
+   var id = programs[sel].id;
+   connection.callService('/get_program', JSON.stringify([id]),
+      function(resp) {
+         editor.setValue(resp.program.code);
+         editor.clearSelection();
+         f.program_name.value = resp.program.info.name;
+         programs[sel] = resp.program.info;
+         update_list(sel);
+      });
+}
+
+// save the program specified on the form
 function save(f) {
-   /*
    var program = {};
-   program.info = {};
+   program.info = programs[f.program.selectedIndex];
    program.info.name = f.program_name.value;
-   program.info.type = 1;
-   // program.info.owner is ignored.
    program.code = editor.getValue();
 
-   connection.callService('/update_program', JSON.stringify([program]),
+   connection.callService('/update_program', 
+         "[" + token + ", " + JSON.stringify(program) + "]",
          null);
-         */
+
+   update_list(f.program.selectedIndex);
 }
 
+// create a new program
 function newprogram(f) {
    var sample = document.getElementById("sample").innerHTML;
    editor.setValue(sample);
@@ -33,59 +52,52 @@ function newprogram(f) {
    f.program_name.value = "New Program";
 
    connection.callService('/create_program', '[' + token + ']',
-         function(resp) {
-            console.log("Create new program: " + resp.id);
-            var program = {};
-            program.info = {};
-            program.code = sample;
-            program.info.id = resp.id;
-            program.info.type = 1;
-            program.info.name = "New Program";
-            programs = [];
-            var p = {};
-            p.token = token;
-            p.program = program;
-            console.log(JSON.stringify([p]));
-            connection.callService('/update_program', JSON.stringify([p]),
-               function(resp) {
-                  console.log("Successfully updated new program");
-                  get_programs();
-               });
-         });
+      function(resp) {
+         console.log("Create new program: " + resp.id);
+         var program = {};
+         program.code = sample;
+         program.info = {};
+         program.info.id = resp.id;
+         program.info.type = 1;
+         program.info.name = "New Program";
+         var sel = programs.length;
+         programs.push(program.info);
+
+         connection.callService('/update_program', 
+            '[' + token + ', ' + JSON.stringify(program) + ']',
+            null);
+
+         update_list(sel);
+      });
 }
 
+// start the editor
 function start_editor() {
    editor = ace.edit("editor");
    var PythonMode = require("ace/mode/python").Mode;
    editor.getSession().setMode(new PythonMode());
 }
 
+// populate the list of programs from the server
 function get_programs() {
    // get program list; display in selection box
    connection.callService('/get_my_programs', '[' + token + ']',
-         function(resp) {
-            // display a program.
-            //  if the last program is save in a cookie, show it
-            //  else, show the first program
-            resp.programs;
-
-            var form = document.getElementById("program_info");
-            form.program.options = [];
-            console.log(form.program.options);
-            programs = [];
-            for (var i = 0 ; i < resp.programs.length ; i++ ) {
-               console.log(resp.programs[i].type);
-               if( resp.programs[i].type === 1 ) {
-                  var opt = document.createElement('option');
-                  opt.text = "(" + resp.programs[i].id + "): " + 
-                     resp.programs[i].name;
-                  opt.value = opt.text;
-                  form.program.add(opt, null);
-                  programs.add(resp.programs[i]);
-               }
+      function(resp) {
+         // display a program.
+         //  if the last program is save in a cookie, show it
+         //  else, show the first program
+         programs = [];
+         for (var i = 0 ; i < resp.programs.length ; i++ ) {
+            // only show python programs
+            if( resp.programs[i].type === 1 ) {
+               programs.push(resp.programs[i]);
             }
+         }
 
-            load(form);
-         });
+         // TODO: load program ID from cookie
+         update_list(0);
 
+         // load the selected program
+         load(document.getElementById("program_info"));
+      });
 }
